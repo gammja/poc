321bdea0af74db4269bcd64d802b9bd50d3ffea1
sprudnikov <Roleshio44>
2016-12-28 00:05:40 +0600

    done

--- html_entity_decode.php
+++ html_entity_decode.php
@@ -8,26 +8,31 @@ class Version
 
     public function setBody($body)
     {
-//        $this->body = htmlentities($body, ENT_QUOTES);
-        $this->body = htmlspecialchars($body, ENT_QUOTES);
-        $this->body = $body;
+        $this->body = htmlentities($body, ENT_QUOTES); //<-- twig escape use the native htmlspecialchars function for the HTML escaping strategy.
+                                                       // vendor/twig/twig/lib/Twig/Extension/Core.php function twig_escape_filter(..)
+
+//        $this->body = $body; if you use twig autoescape option or escape filter with html escaping strategy, remove htmlentities(..)
         return $this;
     }
 
     public function getBody()
     {
-        return $this->body;
-//        return html_entity_decode($this->body);
+        return html_entity_decode($this->body);
+//        return $this->body; <-- you must remove html_entity_decode(..)
     }
 }
 
 //$xss = '<script>alert("xss");</script>';
-$xss = '<b>test</b><br/><img src="#" onerror=alert("xss");>';
-
+$xss = '<img src="#" onerror=alert("xss");>'; // some xss payload
 $version = new Version();
 $version->setBody($xss);
 
-$loader = new Twig_Loader_Filesystem('/');
+$loader = new Twig_Loader_Array(
+    ['index' => '{{ version|raw }}']
+//    ['index' => "{{ version }}"] <-- you can remove raw filter
+);
 $twig = new Twig_Environment($loader);
 
-echo $twig->render('preview_template.html', ['version' => $version->getBody()]);
+echo $twig->render('index',
+    ['version' => $version->getBody()]
+);
